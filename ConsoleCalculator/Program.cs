6ec1eecb7cfaed8dd0ff3d720d530e80efa0e169
nyitraibence <nyitrai100@gmail.com>
2024-02-20 21:39:31 +0000

    C# Console Calculator Done

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,29 +1,53 @@
 ﻿
+do
+{
+    double number1 = 0;
+    double number2 = 0;
+    double result = 0;
 
-double number1 = 0;
-double number2 = 0;
-double result = 0;
+    Console.WriteLine("-----------");
+    Console.WriteLine("C# Calculator");
+    Console.WriteLine("-----------");
 
-Console.WriteLine("-----------");
-Console.WriteLine("C# Calculator");
-Console.WriteLine("-----------");
+    Console.WriteLine("Enter your first number");
+    number1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter your second number");
+    number1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Enter your first number");
-number1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Enter your second number");
-number1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter an option");
+    Console.WriteLine("/t+: ADD ");
+    Console.WriteLine("/t-: SUBTRACT");
+    Console.WriteLine("/t*: MULTIPLY");
+    Console.WriteLine("/t/: DIVIDE");
 
-Console.WriteLine("Enter an option");
-Console.WriteLine("/t+: ADD ");
-Console.WriteLine("/t-: SUBTRACT");
-Console.WriteLine("/t*: MULTIPLY");
-Console.WriteLine("/t/: DIVIDE");
+    switch (Console.ReadLine())
+    {
+        case "+":
+            result = number1 + number2;
+            Console.WriteLine($"{number1} + {number2} = {result}");
+            break;
 
-switch (Console.ReadLine())
-{ 
-    case "+":
-    result = number1 + number2;
-    Console.WriteLine($"{number1} + {number2} = {result}");
-    break;
+        case "-":
+            result = number1 - number2;
+            Console.WriteLine($"{number1} - {number2} = {result}");
+            break;
+
+        case "*":
+            result = number1 * number2;
+            Console.WriteLine($"{number1} * {number2} = {result}");
+            break;
+
+        case "/":
+            result = number1 * number2;
+            Console.WriteLine($"{number1} / {number2} = {result}");
+            break;
+
+        default:
+            Console.WriteLine("You entered a wrong option!");
+            break;
+    }
+    Console.WriteLine("Restart?- Y / N");
     
-}
+} while (Console.ReadLine().ToUpper() == "Y");
+
+Console.WriteLine("Bye!"); 
